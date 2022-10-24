21b6670c2a4187cbc47343ab1b9d44ccfa9b26c9
sbmwangi <shem.mwangi@cellulant.io>
2022-10-24 13:21:40 +0300

    variable declaration and value assigning

---
+++ GettingStarted.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/**
+ *@author Shem Mwangi
+ */
+
+//declare variable type
+var minScore:Int = 10
+
+//type referencing
+var highScore = 0
+highScore = 50
+
+//let is a constant and variable value can't be changed
+let myName = "Constatine"
+
+var currentActiveUsers = 999
+currentActiveUsers = 1400
