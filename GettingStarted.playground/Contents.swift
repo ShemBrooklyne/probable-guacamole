437ffaf0fae26e71103dbdf71a902f1c8caa7777
sbmwangi <shem.mwangi@cellulant.io>
2022-10-24 13:54:25 +0300

    working with Set, trade-offs of why not to use arrays

--- GettingStarted.playground/Contents.swift
+++ GettingStarted.playground/Contents.swift
@@ -39,3 +39,21 @@ ages.insert(36, at: 0) //apending value to desired index
 ages.sort() //sorts existing array in desc-asc order
 print(ages)
 
+/**
+ * Working with Set (considered best in terms of performance & speed over Arrays)
+ */
+
+//comparison between working arrays and set
+
+var randIntArr = [23, 47, 82, 91, 13, 25, 54, 73, 47, 25]
+
+//var randIntSet: Set<Int> = [] //declairing a Set in place of an array
+
+var randIntSet = Set(randIntArr) //omits duplicates in existing array
+
+randIntSet.contains(91) //constant time lookup no matter the size of the array //hashable
+randIntSet.insert(100) //unordered - no duplicates - high performance 
+
+print(randIntSet)
+
+
