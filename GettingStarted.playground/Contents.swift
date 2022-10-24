875f1418599f7944e268ae5b7732c6e283b84537
sbmwangi <shem.mwangi@cellulant.io>
2022-10-24 13:40:24 +0300

    messing around with arrays

--- GettingStarted.playground/Contents.swift
+++ GettingStarted.playground/Contents.swift
@@ -4,10 +4,14 @@ import UIKit
  *@author Shem Mwangi
  */
 
+/**
+ * Variable Declarations
+ */
+
 //declare variable type
 var minScore:Int = 10
 
-//type referencing
+//type inferencing
 var highScore = 0
 highScore = 50
 
@@ -16,3 +20,22 @@ let myName = "Constatine"
 
 var currentActiveUsers = 999
 currentActiveUsers = 1400
+
+/**
+ * Working Arrays
+ */
+
+var ages:[Int] = [23, 18, 24, 12, 43, 33, 27]
+//with type inferencing
+var names = ["Just", "a", "random", "string"]
+
+//returns element count in the ages array
+ages.count
+ages.first
+ages[3] //indexing
+
+ages.append(99) //manipulating existing array by appending new values - always will be at the end.
+ages.insert(36, at: 0) //apending value to desired index
+ages.sort() //sorts existing array in desc-asc order
+print(ages)
+
